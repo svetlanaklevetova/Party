0f0586fd55020a582029773bf6e40676b6fc90ce
Sam Ruby <klevetova91@mail.ru>
2019-04-29 22:55:10 +0300

    start

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+        }
+
+        public ViewResult RvspForm()
+        {
+            return View();
+        }
+
+        
+    }
+}
